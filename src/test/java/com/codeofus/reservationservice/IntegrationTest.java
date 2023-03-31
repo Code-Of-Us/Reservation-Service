f9c5b5f3055e182e26aecead1e259f5f4591f32b
Ivona Raguz <ivona.raguz13@gmail.com>
2023-03-31 09:07:59 +0200

    try to fix the problem with eureka server in test

--- src/test/java/com/codeofus/reservationservice/IntegrationTest.java
+++ src/test/java/com/codeofus/reservationservice/IntegrationTest.java
@@ -4,8 +4,12 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -25,8 +29,9 @@ public class IntegrationTest {
 
     static {
         postgres.start();
-        //eurekaServer.start();
-        //System.setProperty("eureka.client.service-url.defaultZone", "http://" + eurekaServer.getIpAddress() + ":" + eurekaServer.getMappedPort(EUREKA_PORT) + "/eureka");
+        eurekaServer.setWaitStrategy(Wait.defaultWaitStrategy().withStartupTimeout(Duration.of(1, ChronoUnit.MINUTES)));
+        eurekaServer.start();
+        System.setProperty("eureka.client.service-url.defaultZone", "http://" + eurekaServer.getIpAddress() + ":" + eurekaServer.getMappedPort(EUREKA_PORT) + "/eureka");
         System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
         System.setProperty("spring.datasource.username", postgres.getUsername());
         System.setProperty("spring.datasource.password", postgres.getPassword());
