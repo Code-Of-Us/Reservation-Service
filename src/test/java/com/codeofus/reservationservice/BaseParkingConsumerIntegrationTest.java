0e1d77e3904a107f2d8046e6bb9adea67004234d
Ivona Raguz <ivona.raguz13@gmail.com>
2023-04-03 13:33:09 +0200

    add test for CircuitBreaker

---
+++ src/test/java/com/codeofus/reservationservice/BaseParkingConsumerIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.codeofus.reservationservice;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@AutoConfigureWireMock(port = 0)
+public class BaseParkingConsumerIntegrationTest extends IntegrationTest {
+    protected static String CIRCUIT_BREAKER_NAME = "parking";
+
+    @Autowired
+    private CircuitBreakerRegistry registry;
+
+    @BeforeEach
+    public void setup() {
+        WireMock.reset();
+        transitionToState(CircuitBreaker.State.CLOSED);
+    }
+
+    protected void transitionToState(CircuitBreaker.State state) {
+        CircuitBreaker circuitBreaker = registry.circuitBreaker(CIRCUIT_BREAKER_NAME);
+        if (circuitBreaker.getState() != state) {
+            switch (state) {
+                case OPEN -> circuitBreaker.transitionToOpenState();
+                case CLOSED -> circuitBreaker.transitionToClosedState();
+                case HALF_OPEN -> circuitBreaker.transitionToHalfOpenState();
+            }
+        }
+    }
+
+    protected CircuitBreaker.State getCircuitBreakerStatus() {
+        CircuitBreaker circuitBreaker = registry.circuitBreaker(CIRCUIT_BREAKER_NAME);
+        CircuitBreaker.State state = circuitBreaker.getState();
+        return state;
+    }
+
+    protected void stubGetParkingApiToFailWithStatus(int responseCode) {
+        stubFor(get(urlPathEqualTo("/api/v1/parking"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withStatus(responseCode)));
+    }
+
+}
---
+++ src/test/java/com/codeofus/reservationservice/ParkingConsumerTest.java
@@ -0,0 +1,50 @@
+package com.codeofus.reservationservice;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@AutoConfigureWireMock
+@AutoConfigureMockMvc
+public class ParkingConsumerTest extends BaseParkingConsumerIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testCircuitBreakerClosedState() throws Exception {
+        stubGetParkingApiToFailWithStatus(503);
+
+        for (int i = 1; i <= 10; i++) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/reservations/spots"));
+        }
+
+        assertEquals(CircuitBreaker.State.OPEN, getCircuitBreakerStatus());
+    }
+
+    @Test
+    public void testCircuitBreakerMoveToHalfOpenState() throws InterruptedException {
+        transitionToState(CircuitBreaker.State.OPEN);
+        TimeUnit.SECONDS.sleep(61);
+
+        assertEquals(CircuitBreaker.State.HALF_OPEN, getCircuitBreakerStatus());
+    }
+
+    @Test
+    public void testCircuitBreakerMoveToOpenState() throws Exception {
+        stubGetParkingApiToFailWithStatus(503);
+
+        for (int i = 1; i <= 20; i++) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/reservations/spots"));
+        }
+
+        assertEquals(CircuitBreaker.State.OPEN, getCircuitBreakerStatus());
+    }
+}
